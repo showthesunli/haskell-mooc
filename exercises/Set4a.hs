ac1ee13ba7a8e5b7bf50379f724601443250701f
showthesunli <showthesunli@gmail.com>
2022-11-30 00:05:10 +0800

    accomplished ex6

--- exercises/Set4a.hs
+++ exercises/Set4a.hs
@@ -122,8 +122,8 @@ longest = foldr com []
 --   incrementKey True [(True,1),(False,3),(True,4)] ==> [(True,2),(False,3),(True,5)]
 --   incrementKey 'a' [('a',3.4)] ==> [('a',4.4)]
 
-incrementKey :: k -> [(k, v)] -> [(k, v)]
-incrementKey = todo
+incrementKey :: (Eq k, Num v) => k -> [(k, v)] -> [(k, v)]
+incrementKey key = map (\(k, v) -> if key == k then (k, v + 1) else (k, v))
 
 ------------------------------------------------------------------------------
 -- Ex 7: compute the average of a list of values of the Fractional
