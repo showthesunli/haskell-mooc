a7919fb48c8dbb4d47f274c56b994f8ffd0728de
showthesunli <showthesunli@gmail.com>
2022-12-02 01:07:34 +0800

    accomplished ex1-3 in set6

--- exercises/Set6.hs
+++ exercises/Set6.hs
@@ -2,18 +2,21 @@
 
 module Set6 where
 
-import Mooc.Todo
 import Data.Char (toLower)
+import Mooc.Todo
 
 ------------------------------------------------------------------------------
 -- Ex 1: define an Eq instance for the type Country below. You'll need
 -- to use pattern matching.
 
 data Country = Finland | Switzerland | Norway
-  deriving Show
+  deriving (Show)
 
 instance Eq Country where
-  (==) = todo
+  Finland == Finland = True
+  Switzerland == Switzerland = True
+  Norway == Norway = True
+  _ == _ = False
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement an Ord instance for Country so that
@@ -22,10 +25,16 @@ instance Eq Country where
 -- Remember minimal complete definitions!
 
 instance Ord Country where
-  compare = todo -- implement me?
-  (<=) = todo -- and me?
-  min = todo -- and me?
-  max = todo -- and me?
+  compare x y
+    | x == y = EQ
+    | x <= y = LT
+    | otherwise = GT
+  Finland <= _ = True -- and me?
+  _ <= Switzerland = True
+  x <= y = x == y
+
+  min x y = if x <= y then x else y
+  max x y = if x == min x y then y else x -- and me?
 
 ------------------------------------------------------------------------------
 -- Ex 3: Implement an Eq instance for the type Name which contains a String.
@@ -38,10 +47,10 @@ instance Ord Country where
 --   Name "Pekka!" == Name "pekka"  ==> False
 
 data Name = Name String
-  deriving Show
+  deriving (Show)
 
 instance Eq Name where
-  (==) = todo
+  (==) (Name x) (Name y) = and [toLower nx == toLower ny | length x == length y, (nx, ny) <- zip x y]
 
 ------------------------------------------------------------------------------
 -- Ex 4: here is a list type parameterized over the type it contains.
@@ -52,7 +61,7 @@ instance Eq Name where
 -- remove it?
 
 data List a = Empty | LNode a (List a)
-  deriving Show
+  deriving (Show)
 
 instance Eq a => Eq (List a) where
   (==) = todo
@@ -63,18 +72,22 @@ instance Eq a => Eq (List a) where
 -- should return the price of an item.
 --
 -- The prices should be as follows:
+
 -- * chicken eggs cost 20
+
 -- * chocolate eggs cost 30
+
 -- * milk costs 15 per liter
+
 --
 -- Example:
 --   price ChickenEgg  ==>  20
 
 data Egg = ChickenEgg | ChocolateEgg
-  deriving Show
-data Milk = Milk Int -- amount in litres
-  deriving Show
+  deriving (Show)
 
+data Milk = Milk Int -- amount in litres
+  deriving (Show)
 
 ------------------------------------------------------------------------------
 -- Ex 6: define the necessary instance hierarchy in order to be able
@@ -85,7 +98,6 @@ data Milk = Milk Int -- amount in litres
 -- price [Just ChocolateEgg, Nothing, Just ChickenEgg]  ==> 50
 -- price [Nothing, Nothing, Just (Milk 1), Just (Milk 2)]  ==> 45
 
-
 ------------------------------------------------------------------------------
 -- Ex 7: below you'll find the datatype Number, which is either an
 -- Integer, or a special value Infinite.
@@ -94,8 +106,7 @@ data Milk = Milk Int -- amount in litres
 -- and Infinite is greater than any other value.
 
 data Number = Finite Integer | Infinite
-  deriving (Show,Eq)
-
+  deriving (Show, Eq)
 
 ------------------------------------------------------------------------------
 -- Ex 8: rational numbers have a numerator and a denominator that are
@@ -118,7 +129,7 @@ data Number = Finite Integer | Infinite
 --   RationalNumber 13 15 == RationalNumber 4 5  ==> False
 
 data RationalNumber = RationalNumber Integer Integer
-  deriving Show
+  deriving (Show)
 
 instance Eq RationalNumber where
   p == q = todo
@@ -182,7 +193,6 @@ instance Num RationalNumber where
 --   add [1,2] [3,4]        ==>  [1,2,3,4]
 --   add zero [True,False]  ==>  [True,False]
 
-
 ------------------------------------------------------------------------------
 -- Ex 12: cycling. Implement a type class Cycle that contains a
 -- function `step` that cycles through the values of the type.
@@ -210,6 +220,6 @@ instance Num RationalNumber where
 
 data Color = Red | Green | Blue
   deriving (Show, Eq)
+
 data Suit = Club | Spade | Diamond | Heart
   deriving (Show, Eq)
-
--- exercises/playground/ex.hs
+++ exercises/playground/ex.hs
@@ -1,16 +1,42 @@
-import Data.List
+{-# LANGUAGE InstanceSigs #-}
 
-type State = Int
+class Example a where
+  example :: a
 
-newtype ST a = S (State -> (a, State))
+  examples :: [a]
+  examples = [example]
 
-app :: ST Int -> State -> (Int, State)
-app (S st) = st
+instance Example Int where
+  example = 1
 
-instance Functor ST where
-  fmap g (S st) =
-    S
-      ( \s ->
-          let (n, s') = st s
-           in (g n, s')
-      )
+instance Example Bool where
+  example = True
+
+class Size a where
+  size :: a -> Int
+
+instance Size [a] where
+  size :: [a] -> Int
+  size as = length as
+
+class Check a where
+  check :: a -> Bool
+
+instance Check Int where
+  check n = n > 0
+
+instance Check Bool where
+  check b = b
+
+instance Check a => Check [a] where
+  check xs = all check xs
+
+class BitOperations a where
+  bitNot :: a -> a
+  bitNot x = bitNand x x
+  bitAnd :: a -> a -> a
+  bitAnd x y = bitNot (bitOr (bitNot x) (bitNot y))
+  bitOr :: a -> a -> a
+  bitOr x y = bitNot (bitAnd (bitNot x) (bitNot y))
+  bitNand :: a -> a -> a
+  bitNand x y = bitNot (bitAnd x y)
